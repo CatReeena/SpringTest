9bc48dd7b71be1591ae582efc000cbbd10afc5b5
Shera <Katherine-2@yandex.ru>
2017-07-03 16:18:09 +0300

    eclipse example

--- src/main/java/com/company/MyController.java
+++ src/main/java/com/company/MyController.java
@@ -12,12 +12,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MyController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/welcome")
     public ModelAndView helloWorld() {
-        String message = "HELLO SPRING MVC HOW R U";
-        return new ModelAndView("hellopage", "message", message);
+
+        String message = "<br><div style='text-align:center;'>"
+                + "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+        return new ModelAndView("welcome", "message", message);
     }
-
-
-
 }
+
+
+
