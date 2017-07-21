7d69ed0385032987ba39a710c7a97ef869829523
Shera <Katherine-2@yandex.ru>
2017-07-21 18:15:30 +0300

    web.xml changed

--- src/main/java/com/company/AppConfig.java
+++ src/main/java/com/company/AppConfig.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
@@ -8,14 +9,15 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
  * Created by Shera on 07.07.2017.
  */
 @Configuration
+@ComponentScan
 public class AppConfig {
 
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setPersistenceUnitName("Local");
-        return em;
-    }
+//    @Bean
+//    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+//        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+//        em.setPersistenceUnitName("Local");
+//        return em;
+//    }
 
     @Bean
     public CinemaDAOHibernate returnDao()
